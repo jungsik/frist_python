787394620e1dcad9516f1df4265111f72f2415eb
kamui100 <rarsilion@gmail.com>
2020-09-17 00:57:38 +0900

    리스트 함수 확인

--- 20200914/20200914.py
+++ 20200914/20200914.py
@@ -7,3 +7,14 @@ age=[1,2,3]
 name+=age
 
 print(name)
+
+name.append(age)
+
+print(name)
+
+
+name.insert(0,"TTT")
+print(name)
+
+del name[0]
+print(name)
---
+++ 20200917/20200917.py
@@ -0,0 +1,32 @@
+# # a = [1,2,3]
+# # print(a)
+# # b = a
+# # print(b)
+
+# # a[0]='KIM'
+# # print(a)
+# # print(b)
+
+
+# c=[1,2,3]
+# d=c.copy()
+# e=list(c)
+# f=c[::]
+
+# print(d)
+# print(e)
+# print(f)
+
+# c[0]="jungisik"
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+empty_dict={
+    1:"aaa",
+    2:"bbb",
+    3:"ddd",
+}
+print(empty_dict)
+
